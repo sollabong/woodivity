40115759db935a2a78219c0eef192d6faf54ebc2
Gabor Soltesz-Keri <61150302+sollabong@users.noreply.github.com>
2020-09-09 16:07:50 +0200

    javascript to make the main SERVICE container change its content

--- index.js
+++ index.js
@@ -7,3 +7,43 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  function changeServiceNyitott() {
+  document.getElementById("service-main").innerHTML = `
+    <div class="service-image">
+        <i class="fas fa-hammer fa-3x"></i>
+    </div>
+    <h2>NYITOTT MŰHELY</h2>
+    <div class="service-desc-change">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. </div>`;
+  }
+
+  function changeServiceWorkshop() {
+    document.getElementById("service-main").innerHTML = `
+    <div class="service-image">
+        <i class="fas fa-user-cog fa-3x"></i>
+    </div>
+    <h2>WORKSHOPOK</h2>
+    <div class="service-desc-change">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</div>`;
+    }
+
+   function changeServiceGyartas() {
+    document.getElementById("service-main").innerHTML = `
+        <div class="service-image">
+            <i class="fas fa-pencil-ruler fa-3x"></i>
+        </div>
+        <h2>GYÁRTÁS</h2>
+        <div class="service-desc-change">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</div>`;
+   }
+
+   function changeServiceOktatas() {
+    document.getElementById("service-main").innerHTML = `
+        <div class="service-image">
+            <i class="fas fa-chalkboard-teacher  fa-3x"></i>
+        </div>
+        <h2>OKTATÁS, CSAPATÉPÍTÉS</h2>
+        <div class="service-desc-change">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</div>`;
+    }
